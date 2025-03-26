3ccf07c278ce69ac3cd9176a574de2eb9954ab25
FRIDA_ABRIL_CISNEROS_HERNANDEZ <a.22308051281162@cbtis128.edu.mx>
2025-03-26 18:47:26 +0000

    e

---
+++ lib/mi_widgets/mi_tarjeta.dart
@@ -0,0 +1,41 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LaTarjeta extends StatelessWidget {
+  const LaTarjeta({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        child: Card(
+          elevation: 20,
+          color: Colors.amber,
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Abril cisneros 1162",
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text("Tocame"),
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
